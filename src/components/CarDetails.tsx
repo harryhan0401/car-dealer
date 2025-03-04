ed2a8d5f64f9b64f7c15064154778a779f62d2a5
Quoc Han <hhan0014@student.monash.edu>
2025-03-05 04:40:50 +1100

    Add Custom Pagination component

--- src/components/CarDetails.tsx
+++ src/components/CarDetails.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { useAmountRangeParam, useSelectParam } from "@/lib/hooks";
 import VerticalCarCard from "./Cards/VerticalCarCard";
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
+import ProductPagination from "./Pagination";
 
 const CarDetails = ({ cars }: { cars: CarType[] }) => {
+  //Retrieve all query params for car
   const [minPrice, maxPrice] = useAmountRangeParam("amount_range");
   const [minMileage, maxMileage] = useAmountRangeParam("mileage_range");
   console.log(minMileage, maxMileage);
@@ -11,6 +13,11 @@ const CarDetails = ({ cars }: { cars: CarType[] }) => {
   const transmission = useSelectParam("transmission");
   const drive = useSelectParam("drive");
   const [minYear, maxYear] = useAmountRangeParam("year");
+
+  /*
+  Filtering car based on filter applied (useMemo to ensure that list of filtered car 
+  is rerendered when one of the filters applied).
+   */
   const filteredCars = useMemo(() => {
     return cars.filter(
       (car) =>
@@ -37,14 +44,17 @@ const CarDetails = ({ cars }: { cars: CarType[] }) => {
     maxYear,
   ]);
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 6;
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
-      {filteredCars.slice(0, 10).map(({ ...rest }) => (
-        <VerticalCarCard key={Math.random()} {...rest} />
-      ))}
-    </div>
+    <>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
+        {filteredCars.slice(0, 10).map(({ ...rest }) => (
+          <VerticalCarCard key={Math.random()} {...rest} />
+        ))}
+      </div>
+      <div className="mt-5 me-2">
+        <ProductPagination numOfItems={filteredCars.length} itemsPerPage={5} />
+      </div>
+    </>
   );
 };
 export default CarDetails;
---
+++ src/components/Pagination.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import Link from "next/link";
+import { useQueryState } from "nuqs";
+import { useState } from "react";
+
+const ProductPagination = ({
+  numOfItems,
+  itemsPerPage,
+}: {
+  numOfItems: number;
+  itemsPerPage: number;
+}) => {
+  const totalPages = Math.round(numOfItems / itemsPerPage);
+  const [currentPage, setCurrentPage] = useQueryState("page", {
+    defaultValue: 1,
+    parse: (value) => {
+      if (typeof value !== "string" || isNaN(Number(value))) {
+        throw new Error("Invalid page value");
+      }
+      try {
+        const parsedValue = Number(value);
+        if (parsedValue > 0 && parsedValue <= totalPages) {
+          return parsedValue;
+        } else {
+          throw new Error("Page value out of range");
+        }
+      } catch (error) {
+        console.error(`Error parsing page:`, error);
+        return 1;
+      }
+    },
+    serialize: (value) => String(value),
+  });
+
+  return (
+    <div className="flex w-full justify-end">
+      <button
+        onClick={() => currentPage - 1 > 0 && setCurrentPage(currentPage - 1)}
+        disabled={currentPage - 1 == 0}
+        className={`border-2 rounded-l-sm px-2 flex items-center gap-1 cursor-pointer ${currentPage - 1 == 0 && "opacity-50 pointer-events-none"}`}
+      >
+        <ChevronLeft size={20} /> Previous
+      </button>
+      {[...Array(totalPages)].map((_, index) => (
+        <button
+          key={index}
+          className={`px-3 py-2 border-2 cursor-pointer ${currentPage == index + 1 ? "bg-primary text-white" : "bg-transparent"}`}
+          onClick={() => setCurrentPage(index + 1)}
+        >
+          {index + 1}
+        </button>
+      ))}
+      <button
+        onClick={() =>
+          currentPage + 1 <= totalPages && setCurrentPage(currentPage + 1)
+        }
+        disabled={currentPage + 1 > totalPages}
+        className={`border-2 rounded-r-sm px-2 flex items-center gap-1 cursor-pointer ${currentPage + 1 > totalPages && "opacity-50 pointer-events-none"}`}
+      >
+        Next <ChevronRight size={20} />
+      </button>
+    </div>
+  );
+};
+
+export default ProductPagination;
